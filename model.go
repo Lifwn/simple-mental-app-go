99d1b488e95db47f0ba0c2e9e0d578c10a3c8344
Lifwn <alifwiraseno.n@gmail.com>
2026-08-05 11:36:28 +0700

    refactor code

--- model.go
+++ model.go
@@ -1,15 +1,26 @@
 package main
 
-import "time"
-
+import (
+	"fmt"
+	"time"
+)
 
 type Assessment struct {
-	IDUser      string
-	Tanggal     time.Time
-	Jawaban     [10]int 
-	SkorTotal   int
+	IDUser    string
+	Tanggal   time.Time
+	Jawaban   [10]int
+	SkorTotal int
 }
 
+func (a *Assessment) Print(title string) {
+	if title != "" {
+		fmt.Println(title)
+	}
+	fmt.Printf("ID          : %s\n", a.IDUser)
+	fmt.Printf("Tanggal     : %s\n", a.Tanggal.Format("2006-01-02"))
+	fmt.Printf("Skor        : %d\n", a.SkorTotal)
+	fmt.Printf("Interpretasi: %s\n", InterpretasiSkor(a.SkorTotal))
+}
 
 const MaxData = 100
 var dataAssessment [MaxData]Assessment
